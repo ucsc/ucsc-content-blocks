ca863d5869f804dac7627d78d79a86d4767639a5
Jason Chafin <jchafin@ucsc.edu>
2023-08-01 08:42:59 -0700

    chore: :bookmark: Bump WP version in plugin header

--- ucsc-content-blocks.php
+++ ucsc-content-blocks.php
@@ -5,7 +5,7 @@
  * Description:       Content blocks for UCSC WordPress Websites.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.8
+ * Version:           0.1.9
  * Author:            UC Santa Cruz
  * Author URI:        https://github.com/ucsc
  * License:           GPL-2.0-or-later
