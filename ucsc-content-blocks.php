d58b565cb241bfc62ce44073f86ed329d610df39
Jason Chafin <jchafin@ucsc.edu>
2023-08-21 07:20:43 -0700

    chore: :memo: Bump version in `ucsc-content-blocks.php`

--- ucsc-content-blocks.php
+++ ucsc-content-blocks.php
@@ -5,7 +5,7 @@
  * Description:       Content blocks for UCSC WordPress Websites.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.9
+ * Version:           0.1.10-beta.0
  * Author:            UC Santa Cruz
  * Author URI:        https://github.com/ucsc
  * License:           GPL-2.0-or-later
